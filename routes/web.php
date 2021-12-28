615e90eaecf0546c3ed7baa4707911a5bf447cdd
Amol Wankhede <amolsw.nz@gmail.com>
2021-12-28 13:39:46 +0530

    Vue 2 scafold done

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/vue2', function () {
+    return view('vue2');
+});
