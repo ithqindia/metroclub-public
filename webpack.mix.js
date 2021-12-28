aa5d44f72bbf8f3658bed41e4cde40b8e79c46f1
Amol Wankhede <amolsw.nz@gmail.com>
2021-12-28 13:35:49 +0530

    Initial commit clean repo vue 2

---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require("laravel-mix");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js("resources/js/app.js", "public/js")
+    .js("resources/js/vue2/vue2.js", "public/")
+
+    .vue({ version: 2 })
+    .browserSync("http://apply2campus-v2.test/")
+    .sourceMaps();
