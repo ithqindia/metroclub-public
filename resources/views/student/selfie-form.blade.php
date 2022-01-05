62fceda5c2692d0a9f76393f2ab2c5f5212f8c38
MANISHA NAYAK <nayakmanisha515@gmail.com>
2022-01-05 14:16:17 +0530

    selfie

--- resources/views/student/selfie-form.blade.php
+++ resources/views/student/selfie-form.blade.php
@@ -22,13 +22,13 @@
         {{-- image --}}
         <div class="row">
             <div class="col-4">
-                <img src="/assets/lefthandonsholder.jpeg" width="200" height="400" class="rounded float-right" alt="image1">
+                <img src="\assets\lefthandonsholder.jpeg" width="200" height="400" class="rounded float-right" alt="image1">
             </div>
             <div class="col-4">
-                <img src="/assets/sholder.jpeg" width="200" height="400" class="rounded float-right" alt="image2">
+                <img src="\assets\sholder.jpeg" width="200" height="400" class="rounded float-right" alt="image2">
             </div>
             <div class="col-4">
-                <img src="/assets/lefthandonchest.jpeg" width="200" height="400" class="rounded float-right" alt="image3">
+                <img src="\assets\lefthandonchest.jpeg" width="200" height="400" class="rounded float-right" alt="image3">
             </div>
         </div>
         <form method="post" action="/me/selfie">
@@ -36,21 +36,34 @@
                 {{ method_field('PUT') }}
             @endisset
             @csrf
-            <div class="form-group">
-                <div class="col my-5">
-                    <label for="selfie">Selfie</label>
-                </div>
-                <input type="file" aria-describedby="SelfieHelp" class="form-control @error('selfie') is-invalid @enderror"
-                    name="selfie" value="{{ old('selfie', $selfieInformation->selfie ?? '') }}">
-                @error('selfie')
-                    <div class="invalid-feedback">
-                        {{ $message }}
+                <div class="row">
+                    <div class="col-mb-12 mt-4">
+                        <div class="image-input image-input-empty" data-kt-image-input="true"
+                            style="background-image: url(https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png)">
+                            <div class="image-input-wrapper w-125px h-125px"></div>
+                            <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-white shadow"
+                                data-kt-image-input-action="change" data-bs-toggle="tooltip" data-bs-dismiss="click"
+                                title="Change avatar">
+                                <i class="bi bi-pencil-fill fs-7"></i>
+                                <input type="file" name="selfie" accept=".png, .jpg, .jpeg" />
+                                <input type="hidden" name="selfie_remove" />
+                            </label>
+
+                            <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-white shadow"
+                                data-kt-image-input-action="cancel" data-bs-toggle="tooltip" data-bs-dismiss="click"
+                                title="Cancel avatar">
+                                <i class="bi bi-x fs-2"></i>
+                            </span>
+
+                            <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-white shadow"
+                                data-kt-image-input-action="remove" data-bs-toggle="tooltip" data-bs-dismiss="click"
+                                title="Remove selfie">
+                                <i class="bi bi-x fs-2"></i>
+                            </span>
+                        </div>
                     </div>
-                @enderror
-                <small id="selfieHelp" class="form-text text-muted">PDF,JPG or PNG format and please do not exceed
-                    5MB</small>
-            </div>
-            <button type="submit" class="btn btn-primary">Upload image</button>
+                </div>
+                <button type="submit" class="btn btn-primary">Upload image</button>
         </form>
         </div>
     </div>
