183c3a6fd640fdf63d849ac73e3482ba06a8db0f
ITHQ India <ithqindia@gmail.com>
2022-01-07 09:04:46 +0530

    ------amend

--- resources/views/single/login.blade.php
+++ resources/views/single/login.blade.php
@@ -30,33 +30,40 @@
                 <div class="col-md-1 d-sm-block"></div>
                 <div class="col-md-5 col-sm-12">
                     <div class="bg-body rounded shadow-sm mx-10 p-10 p-lg-15">
-                        <form class="form " novalidate="novalidate" id="kt_sign_up_form">
+                        <form method="POST" action="/register">
+                            @csrf
                             <div class="mb-10 text-center">
                                 <h1 class="text-dark mb-3">Create an account</h1>
                             </div>
+                            <div>
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                            </div>
                             <div class="row fv-row mb-7">
-                                <div class="col-xl-6">
-                                    <label class="form-label fw-bolder text-dark fs-6">First Name</label>
+                                <div class="col-xl-12">
+                                    <label class="form-label fw-bolder text-dark fs-6">Your name</label>
                                     <input class="form-control form-control-lg form-control-solid" type="text"
-                                        placeholder="" name="first-name" autocomplete="off" />
-                                </div>
-                                <div class="col-xl-6">
-                                    <label class="form-label fw-bolder text-dark fs-6">Last Name</label>
-                                    <input class="form-control form-control-lg form-control-solid" type="text"
-                                        placeholder="" name="last-name" autocomplete="off" />
+                                        name="name" autocomplete="off" />
                                 </div>
                             </div>
                             <div class="fv-row mb-7">
                                 <label class="form-label fw-bolder text-dark fs-6">Email</label>
-                                <input class="form-control form-control-lg form-control-solid" type="email"
-                                    placeholder="" name="email" autocomplete="off" />
+                                <input class="form-control form-control-lg form-control-solid" type="email" name="email"
+                                    autocomplete="off" />
                             </div>
                             <div class="mb-10 fv-row" data-kt-password-meter="true">
                                 <div class="mb-1">
                                     <label class="form-label fw-bolder text-dark fs-6">Password</label>
                                     <div class="position-relative mb-3">
                                         <input class="form-control form-control-lg form-control-solid" type="password"
-                                            placeholder="" name="password" autocomplete="off" />
+                                            name="password" autocomplete="off" />
                                         <span
                                             class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2"
                                             data-kt-password-meter-control="visibility">
@@ -82,7 +89,7 @@
                             <div class="fv-row mb-5">
                                 <label class="form-label fw-bolder text-dark fs-6">Confirm Password</label>
                                 <input class="form-control form-control-lg form-control-solid" type="password"
-                                    placeholder="" name="confirm-password" autocomplete="off" />
+                                    name="password_confirmation" autocomplete="off" />
                             </div>
                             <div class="fv-row mb-10">
                                 <label class="form-check form-check-custom form-check-solid form-check-inline">
@@ -92,7 +99,7 @@
                                 </label>
                             </div>
                             <div class="text-center">
-                                <button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-primary w-100 mb-5">
+                                <button type="submit" class="btn btn-lg btn-primary w-100 mb-5">
                                     <span class="indicator-label">Register</span> </button>
                             </div>
                         </form>
@@ -101,8 +108,8 @@
                 <div class="col-md-5 col-sm-12">
                     <div class="mx-10">
                         <div class="bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
-                            <form class="form fv-plugins-bootstrap5 fv-plugins-framework" novalidate="novalidate"
-                                id="kt_sign_in_form" action="#">
+                            <form id="kt_sign_in_form" method="POST" action="/login">
+                                @csrf
                                 <div class="text-center mb-10">
                                     <h1 class="text-dark mb-3">Login to your account</h1>
                                 </div>
@@ -115,7 +122,7 @@
                                 <div class="fv-row mb-10 fv-plugins-icon-container">
                                     <div class="d-flex flex-stack mb-2">
                                         <label class="form-label fw-bolder text-dark fs-6 mb-0">Password</label>
-                                        <a href="../../demo2/dist/authentication/flows/basic/password-reset.html"
+                                        <a href="{{ route('password.request') }}"
                                             class="link-primary fs-6 fw-bolder">Forgot Password ?</a>
                                     </div>
                                     <input class="form-control form-control-lg form-control-solid" type="password"
@@ -123,8 +130,7 @@
                                     <div class="fv-plugins-message-container invalid-feedback"></div>
                                 </div>
                                 <div class="text-center">
-                                    <button type="submit" id="kt_sign_in_submit"
-                                        class="btn btn-lg btn-primary w-100 mb-5"> <span
+                                    <button type="submit" class="btn btn-lg btn-primary w-100 mb-5"> <span
                                             class="indicator-label">Login</span> </button>
                                 </div>
                             </form>
